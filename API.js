68510cb44c26b05c94aa11c20aeb7ff56290bf12
JoseDCaballero <22090601@alumno.utmetropolitana.edu.mx>
2023-07-30 01:03:58 -0600

    first commit

--- API.js
+++
@@ -1,8 +0,0 @@
-var API_KEY = '38540858-7e235397a199eaf533c2ff91e';
-var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('red roses');
-$.getJSON(URL, function(data){
-if (parseInt(data.totalHits) > 0)
-    $.each(data.hits, function(i, hit){ console.log(hit.pageURL); });
-else
-    console.log('No hits');
-});
